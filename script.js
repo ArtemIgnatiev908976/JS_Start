b6d3ca2f20679d49fce6af7342baa57a4a40c008
Artem <ignatev.artem.nik@gmail.com>
2021-03-11 23:39:35 +0400

    Циклы урок 1

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-const personalMovieDB ={
-    count: numberOfFilms,
-    movies:{},
-    actors:{},
-    generals:[],
-    private: false
-};
+//
+// ///1 задание
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const personalMovieDB ={
+//     count: numberOfFilms,
+//     movies:{},
+//     actors:{},
+//     generals:[],
+//     private: false
+// };
+//
+// const a = prompt('Один из последних просмотренных фильмов?',''),
+//       b = prompt('На сколько оцените его?',''),
+//       c = prompt('Один из последних просмотренных фильмов?',''),
+//       d = prompt('На сколько оцените его?','');
+//
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+//
+// console.log(personalMovieDB)
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько оцените его?','');
+//3 задание
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+let num = 50;
+// while (num <= 55){
+//     console.log(num);
+//     num++;
+//
+// };
 
-console.log(personalMovieDB)
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num <55);
+
+for (let i = 1; i <10;i++){
+  if(i===6){
+     continue;
+  }
+  console.log(i)
+}
