bf905cb9840f546aa31fdca1a1846d5fdfebde8b
Artem <ignatev.artem.nik@gmail.com>
2021-03-14 00:04:58 +0400

    Методы и свойства строк и чисел

--- script.js
+++ script.js
@@ -127,7 +127,7 @@
 
 
 
-// //Функция
+// // //Функция
 // "use strict";
 // let num = 20;
 // function showFirstMessage(text){
@@ -164,6 +164,35 @@
 // logger();
 //
 //
+//
+//
+// const calc =(a,b) =>  {return a + b};
+
+const str = "teSt";
+
+console.log(str.toLocaleUpperCase());
+console.log(str.toLocaleLowerCase());
 
 
-const calc =(a,b) =>  {return a + b};
+let fruit ="Some fruit";
+
+console.log(fruit.indexOf("fruit"))
+
+
+const logg = "Hello world";
+
+console.log(logg.slice(6, 12 ));
+console.log(logg.substring(6, 12 ));
+console.log(logg.substr(6, 5 ));
+
+
+const num = 12.2242424;
+
+console.log(Math.round(num))
+
+
+
+const test = "12.2123px";
+// console.log(parseInt(test));
+console.log(parseFloat(test));
+
