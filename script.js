89df4312c67e463ff6fbbee64fd1a3bff6b83923
Artem <ignatev.artem.nik@gmail.com>
2021-03-15 00:05:01 +0400

    Используем функции

--- script.js
+++ script.js
@@ -168,31 +168,136 @@
 //
 // const calc =(a,b) =>  {return a + b};
 
-const str = "teSt";
+// const str = "teSt";
+//
+// console.log(str.toLocaleUpperCase());
+// console.log(str.toLocaleLowerCase());
+//
+//
+// let fruit ="Some fruit";
+//
+// console.log(fruit.indexOf("fruit"))
+//
+//
+// const logg = "Hello world";
+//
+// console.log(logg.slice(6, 12 ));
+// console.log(logg.substring(6, 12 ));
+// console.log(logg.substr(6, 5 ));
+//
+//
+// const num = 12.2242424;
+//
+// console.log(Math.round(num))
+//
+//
+//
+// const test = "12.2123px";
+// // console.log(parseInt(test));
+// console.log(parseFloat(test));
 
-console.log(str.toLocaleUpperCase());
-console.log(str.toLocaleLowerCase());
+///1 задание
+let numberOfFilms ;
 
 
-let fruit ="Some fruit";
-
-console.log(fruit.indexOf("fruit"))
-
-
-const logg = "Hello world";
-
-console.log(logg.slice(6, 12 ));
-console.log(logg.substring(6, 12 ));
-console.log(logg.substr(6, 5 ));
-
-
-const num = 12.2242424;
-
-console.log(Math.round(num))
+function start(){
 
 
 
-const test = "12.2123px";
-// console.log(parseInt(test));
-console.log(parseFloat(test));
+    while (numberOfFilms=='' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    }
+
+}
+start();
+
+
+const personalMovieDB ={
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    generals:[],
+    private: true
+};
+
+
+
+
+
+
+
+
+function rememberMyFilms(){
+
+    for(let i =0; i<2; i++){
+
+        const a = prompt('Один из последних просмотренных фильмов?',''),
+            b = prompt('На сколько оцените его?','');
+
+        if (a !=null && b !=null  && a!='' && b !='' && a.length < 50){
+
+            personalMovieDB.movies[a] = b;
+            console.log('done')
+        } else{
+            console.log('error');  // если не правильно заполнили все заного
+            i--;
+        }
+
+
+
+    }
+
+
+}
+
+rememberMyFilms();
+
+
+
+
+
+
+function detectPersonalLevel(){
+    if (personalMovieDB.count < 10){
+        console.log('Просмотрено довольно мало фильмов');
+    }
+    else if(personalMovieDB.count >=10 && personalMovieDB.count <30){
+        console.log('Вы класический зритель');
+    }
+    else if (personalMovieDB.count >=30){
+        console.log('Вы киноман');
+    }
+    else {
+        console.log('Произошла ошибка');
+    }
+
+}
+
+detectPersonalLevel();
+
+
+
+
+function showMyDB(hidden){
+if(!hidden){
+    console.log(personalMovieDB.private)
+}
+
+}
+
+showMyDB(personalMovieDB);
+
+function writeYouGenerals(){
+    for (let i = 1; i <= 3; i++){
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+       personalMovieDB.generals[i-1] = genre;
+    }
+}
+
+writeYouGenerals();
+
+
+
+
 
