68215cc2a6563dd2b358b354f149d4dcc695233c
Artem <ignatev.artem.nik@gmail.com>
2021-04-09 14:07:05 +0400

    Инкапсуляция геттер и сеттер

--- Инкапсуляция/script.js
+++ Инкапсуляция/script.js
@@ -1,23 +1,32 @@
 'use strict';
 
-function User(name, age){
-    this.name = name;
-    this.age = age;
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
+    }
 
+    #surname = 'Petrychenko';
 
-    this.say = function (){
-        console.log(`Имя пользователя: ${this.name}, возраст ${this.age}`)
-    };
+    say = () => {
+        console.log(`Имя пользователя: ${this.name} ${this.#surname}, возраст ${this._age}`);
+    }
 
+    get age() {
+        return this._age;
+    }
+
+    set age(age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            this._age = age;
+        } else {
+            console.log('Недопустимое значение!');
+        }
+    }
 }
 
+const ivan = new User('Ivan', 27);
 
-const ivan = new User('Ivan, 27');
 
-console.log(ivan.name);
-console.log(ivan.age);
-
-ivan.age = 30;
-ivan.name = 'Alex';
-
+console.log(ivan.surname);
 ivan.say();
